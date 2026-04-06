233ab304365a134690feb10d11b8876472e23a63
christopher-sanchez <csanchez@my.okcu.edu>
2026-04-06 11:53:59 -0500

    attempt 2

--- Fib/fib.js
+++ Fib/fib.js
@@ -1,15 +1,16 @@
 
 function fibs(Num_of_fibs){
-    Num_of_fibs = 8;
-    n1 = 0;
-    n2 = 1;
-    for(let i=0; Num_of_fibs>i; i++){
-        
-        print(n1 + " ");
-        
+     let n1 = 0;
+     let n2 = 1;
+     let n3 = 0;
+    for(let i=1; Num_of_fibs>i; i++){
+        console.log(n1 + " ");
          n3 = n1+n2;
-         n1 = n2;
+          n1 = n2;
          n2 = n3;
     }
+    return n1;
 
+    
 }
+ console.log(fibs(8));
