db401cb5a4258f80f2e27569877a2d378129e628
christopher-sanchez <csanchez@my.okcu.edu>
2026-04-06 16:59:32 -0500

    recur attempt

--- Fib/fib.js
+++ Fib/fib.js
@@ -6,11 +6,22 @@ function fibs(Num_of_fibs){
     for(let i=1; Num_of_fibs>i; i++){
         console.log(n1 + " ");
          n3 = n1+n2;
-          n1 = n2;
+        n1 = n2;
          n2 = n3;
     }
     return n1;
 
     
+}
+
+function recurfibs(Num_of_fibs){
+    let n1 = 0;
+    let n2 = 1;
+    let n3 = 0;
+    n3 = n1+n2;
+    n1=n2;
+    n2=n3;
+    recurfibs;
+    return n1;
 }
  console.log(fibs(8));
